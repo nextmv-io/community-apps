2a4ca61575ae24bb83a95001fc2db046daad657f
Sebastian Quintero <sebastian@nextmv.io>
2024-01-17 15:51:04 -0500

    Add the code from templates

---
+++ mip/README.md
@@ -0,0 +1,41 @@
+# Nextmv MIP template
+
+This template demonstrates how to solve a Mixed Integer Programming problem
+using the open-source solver [HiGHS](https://github.com/ERGO-Code/HiGHS).
+
+To solve a Mixed Integer Problem (MIP) is to optimize a linear objective
+function of many variables, subject to linear constraints. We demonstrate this
+by solving the knapsack problem.
+
+Knapsack is a classic combinatorial optimization problem. Given a collection of
+items with a value and weight, our objective is to maximize the total value
+without exceeding the weight capacity of the knapsack.
+
+The input defines a number of items which have an id to identify the item, a
+weight and a value. Additionally there is a weight capacity.
+
+The most important files created are `main.go` and `input.json`.
+
+* `main.go` implements a MIP knapsack solver.
+* `input.json` is a sample input file that follows the input definition in
+`main.go`.
+
+Run the command below to check that everything works as expected:
+
+```bash
+nextmv sdk run . -- -runner.input.path input.json \
+  -runner.output.path output.json -solve.duration 10s
+```
+
+A file `output.json` should have been created with the optimal knapsack
+solution.
+
+## Next steps
+
+* Open `main.go` and read through the comments to understand the model.
+* API documentation and examples can be found in the [package
+  documentation](https://pkg.go.dev/github.com/nextmv-io/sdk/mip).
+* Further documentation, guides, and API references about custom modeling and
+  deployment can also be found on our [blog](https://www.nextmv.io/blog) and on
+  our [documentation site](https://docs.nextmv.io).
+* Need more assistance? Send us an [email](mailto:support@nextmv.io)!
---
+++ pyomo/main.py
@@ -0,0 +1,167 @@
+"""
+Template for working with Pyomo.
+"""
+
+import argparse
+import json
+import sys
+from typing import Any
+
+import pyomo.environ as pyo
+
+# Duration parameter for the solver.
+SUPPORTED_PROVIDER_DURATIONS = {
+    "cbc": "sec",
+    "glpk": "tmlim",
+}
+
+
+# Status of the solver after optimizing.
+STATUS = {
+    pyo.TerminationCondition.feasible: "suboptimal",
+    pyo.TerminationCondition.infeasible: "infeasible",
+    pyo.TerminationCondition.optimal: "optimal",
+    pyo.TerminationCondition.unbounded: "unbounded",
+}
+
+
+def main() -> None:
+    """Entry point for the template."""
+
+    parser = argparse.ArgumentParser(description="Solve problems with Pyomo.")
+    parser.add_argument(
+        "-input",
+        default="",
+        help="Path to input file. Default is stdin.",
+    )
+    parser.add_argument(
+        "-output",
+        default="",
+        help="Path to output file. Default is stdout.",
+    )
+    parser.add_argument(
+        "-duration",
+        default=30,
+        help="Max runtime duration (in seconds). Default is 30.",
+        type=int,
+    )
+    parser.add_argument(
+        "-provider",
+        default="cbc",
+        help="Solver provider. Default is cbc.",
+    )
+    args = parser.parse_args()
+
+    # Read input data, solve the problem and write the solution.
+    input_data = read_input(args.input)
+    log("Solving knapsack problem:")
+    log(f"  - items: {len(input_data.get('items', []))}")
+    log(f"  - capacity: {input_data.get('weight_capacity', 0)}")
+    log(f"  - max duration: {args.duration} seconds")
+    solution = solve(input_data, args.duration, args.provider)
+    write_output(args.output, solution)
+
+
+def solve(input_data: dict[str, Any], duration: int, provider: str) -> dict[str, Any]:
+    """Solves the given problem and returns the solution."""
+
+    # Make sure the provider is supported.
+    if provider not in SUPPORTED_PROVIDER_DURATIONS:
+        raise ValueError(
+            f"Unsupported provider: {provider}. The supported providers are: "
+            f"{', '.join(SUPPORTED_PROVIDER_DURATIONS.keys())}"
+        )
+
+    # Creates the model.
+    model = pyo.ConcreteModel()
+
+    # Creates the solver.
+    solver = pyo.SolverFactory(provider)
+    solver.options[SUPPORTED_PROVIDER_DURATIONS[provider]] = duration
+
+    # Initializes the linear sums.
+    weights = 0.0
+    values = 0.0
+
+    # Creates the decision variables.
+    item_ids = [item["id"] for item in input_data["items"]]
+    model.item_variable = pyo.Var(item_ids, domain=pyo.Boolean)
+
+    # Use the decision variables for the linear sums
+    items = []
+    for item in input_data["items"]:
+        item_id = item["id"]
+        items.append({"item": item, "variable": model.item_variable[item_id]})
+        weights += model.item_variable[item_id] * item["weight"]
+        values += model.item_variable[item_id] * item["value"]
+
+    # This constraint ensures the weight capacity of the knapsack will not be
+    # exceeded.
+    model.constraint = pyo.Constraint(expr=weights <= input_data["weight_capacity"])
+
+    # Sets the objective function: maximize the value of the chosen items.
+    model.objective = pyo.Objective(expr=values, sense=pyo.maximize)
+
+    # Solves the problem.
+    results = solver.solve(model)
+
+    # Determines which items were chosen.
+    chosen_items = [item["item"] for item in items if item["variable"]() > 0.9]
+
+    # Creates the statistics.
+    statistics = {
+        "result": {
+            "custom": {
+                "constraints": model.nconstraints(),
+                "provider": provider,
+                "status": STATUS.get(results.solver.termination_condition, "unknown"),
+                "variables": model.nvariables(),
+            },
+            "duration": results.solver.time,
+            "value": pyo.value(model.objective),
+        },
+        "run": {
+            "duration": results.solver.time,
+        },
+        "schema": "v1",
+    }
+
+    return {
+        "solutions": [{"items": chosen_items}],
+        "statistics": statistics,
+    }
+
+
+def log(message: str) -> None:
+    """Logs a message. We need to use stderr since stdout is used for the
+    solution."""
+
+    print(message, file=sys.stderr)
+
+
+def read_input(input_path) -> dict[str, Any]:
+    """Reads the input from stdin or a given input file."""
+
+    input_file = {}
+    if input_path:
+        with open(input_path, encoding="utf-8") as file:
+            input_file = json.load(file)
+    else:
+        input_file = json.load(sys.stdin)
+
+    return input_file
+
+
+def write_output(output_path, output) -> None:
+    """Writes the output to stdout or a given output file."""
+
+    content = json.dumps(output, indent=2)
+    if output_path:
+        with open(output_path, "w", encoding="utf-8") as file:
+            file.write(content + "\n")
+    else:
+        print(content)
+
+
+if __name__ == "__main__":
+    main()
