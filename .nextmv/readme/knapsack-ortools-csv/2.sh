f987d10dcf24aaf5c69e630b77321c6388df4542
Marius Merschformann <marius.merschformann@gmail.com>
2024-05-24 02:28:25 +0200

    Updating test expectation

--- .nextmv/readme/knapsack-ortools-csv/2.sh
+++ .nextmv/readme/knapsack-ortools-csv/2.sh
@@ -1,3 +1,3 @@
 docker run -i --rm \
 -v $(pwd):/app ghcr.io/nextmv-io/runtime/python:3.11 \
-sh -c 'pip install -r requirements.txt && python3 /app/main.py'
+sh -c 'pip install -r requirements.txt > /dev/null && python3 /app/main.py'
