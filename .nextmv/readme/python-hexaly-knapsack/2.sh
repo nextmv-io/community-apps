2d5f188ab6bdc1a9bf1c3af5d1e09aed33f222ae
Sebastian Quintero <sebastian@nextmv.io>
2024-09-25 12:32:25 -0500

    Update README test command for Hexaly

--- .nextmv/readme/python-hexaly-knapsack/2.sh
+++ .nextmv/readme/python-hexaly-knapsack/2.sh
@@ -1,3 +1,3 @@
 cat input.json | docker run -i --rm \
 -v $(pwd):/app ghcr.io/nextmv-io/runtime/hexaly:latest \
-sh -c 'pip install -r requirements.txt > /dev/null && python3 /app/main.py'
+sh -c 'python3 /app/main.py'
