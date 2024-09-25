d699df94d457de3165ef6cb94f5da73b7aa5f871
Ryan O'Neil <6748953+ryanjoneil@users.noreply.github.com>
2024-09-25 11:02:07 -0400

    Adds statistics to Python hello world example.

--- python-hello-world/main.py
+++ python-hello-world/main.py
@@ -1,14 +1,20 @@
 import nextmv
 
-# Read from stdin.
-input = nextmv.load_local()
-name = input.data["name"]
+if __name__ == "__main__":
+    # Read from stdin.
+    input = nextmv.load_local()
+    name = input.data["name"]
 
-##### Insert model here
+    ##### Insert model here
 
-# Print logs that render in the run view in Nextmv Console
-nextmv.log(f"Hello, {name}")
+    # Print logs that render in the run view in Nextmv Console
+    nextmv.log(f"Hello, {name}")
 
-# Write output and statistics.
-output = nextmv.Output(solution=None, statistics={"message": f"Hello, {name}"})
-nextmv.write_local(output)
+    # Write output and statistics to stdout.
+    output = nextmv.Output(
+        solution=f"Hello, {name}",
+        statistics=nextmv.Statistics(
+            result=nextmv.ResultStatistics(custom={"length": len(name)}),
+        ),
+    )
+    nextmv.write_local(output)
