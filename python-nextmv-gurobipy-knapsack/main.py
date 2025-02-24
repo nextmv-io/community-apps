7ce697eb30aa8e4e1776554baf2fe037c8633793
Sebastian Quintero <sebastianquinterorojas@gmail.com>
2025-02-24 16:50:48 -0500

    Adds the community apps for nextmv-scikit-learn and nextmv-gurobipy

---
+++ python-nextmv-gurobipy-knapsack/main.py
@@ -0,0 +1,66 @@
+import time
+
+import nextmv
+import nextmv_gurobipy as ngp
+from gurobipy import GRB
+
+
+def main() -> None:
+    """Entry point for the program."""
+
+    opt = nextmv.Options(
+        nextmv.Parameter("input", str, "", "Path to input file. Default is stdin.", False),
+        nextmv.Parameter("output", str, "", "Path to output file. Default is stdout.", False),
+    )
+    gp_opt = ngp.ModelOptions().to_nextmv()
+    options = opt.merge(gp_opt)
+
+    input = nextmv.load_local(options=options, path=options.input)
+
+    nextmv.log("Solving knapsack problem:")
+    nextmv.log(f"  - items: {len(input.data.get('items', []))}")
+    nextmv.log(f"  - capacity: {input.data.get('weight_capacity', 0)}")
+
+    model = DecisionModel()
+    output = model.solve(input)
+    nextmv.write_local(output, path=options.output)
+
+
+class DecisionModel(nextmv.Model):
+    def solve(self, input: nextmv.Input) -> nextmv.Output:
+        """Solves the given problem and returns the solution."""
+
+        start_time = time.time()
+        model = ngp.Model(input.options)
+
+        # Initializes the linear sums.
+        weights = 0.0
+        values = 0.0
+
+        # Creates the decision variables and adds them to the linear sums.
+        items = []
+        for item in input.data["items"]:
+            item_variable = model.addVar(vtype=GRB.BINARY, name=item["id"])
+            items.append({"item": item, "variable": item_variable})
+            weights += item_variable * item["weight"]
+            values += item_variable * item["value"]
+
+        # This constraint ensures the weight capacity of the knapsack will not be
+        # exceeded.
+        model.addConstr(weights <= input.data["weight_capacity"])
+
+        # Sets the objective function: maximize the value of the chosen items.
+        model.setObjective(expr=values, sense=GRB.MAXIMIZE)
+
+        # Solves the problem.
+        model.optimize()
+
+        return nextmv.Output(
+            options=input.options,
+            solution=ngp.ModelSolution(model),
+            statistics=ngp.ModelStatistics(model, start_time),
+        )
+
+
+if __name__ == "__main__":
+    main()
---
+++ python-nextmv-scikit-learn-diabetes/README.md
@@ -0,0 +1,24 @@
+# Nextmv & scikit-learn Diabetes
+
+This community app contains a Jupyter Notebook that demonstrates how to use the
+`nextmv-scikit-learn` package, which was created to facilitate the integration
+of Nextmv and scikit-learn.
+
+The classical diabetes dataset is used with different regressors to:
+
+1. train a scikit-learn model,
+2. fit it,
+3. export the fitted model (solution),
+4. import the model,
+5. make predictions.
+
+The notebook showcases how to achieve this, step by step.
+
+## Next steps
+
+* Visit our [docs][docs] and [blog][blog]. Need more assistance?
+  [Contact][contact] us!
+
+[docs]: https://docs.nextmv.io
+[blog]: https://www.nextmv.io/blog
+[contact]: https://www.nextmv.io/contact
