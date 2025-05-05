94eb4556dbf6fa46006fd029481aaeeae2619011
mooneyc6 <carolyn.mooney@gmail.com>
2025-05-05 13:38:13 -0400

    Fix linting

--- python-wf-databricks/main.py
+++ python-wf-databricks/main.py
@@ -1,12 +1,10 @@
+# Standard library imports
 import json
 
+# Third-party imports
 import nextmv
-import nextmv.cloud as cloud
-import os
-from nextpipe import FlowSpec, app, needs, step
 from databricks.sdk import WorkspaceClient
-from databricks.sdk.service import jobs
-import time
+from nextpipe import FlowSpec, needs, step
 
 # Option to pass a Databricks job ID to run
 options = nextmv.Options(
@@ -22,16 +20,17 @@ class DecisionFlow(FlowSpec):
 
         db_job_id = options.db_job_id
 
-        # Authenticate (assumes DATABRICKS_HOST and DATABRICKS_TOKEN env vars are set as Nextmv secrets)
+        # Authenticate (assumes DATABRICKS_HOST and DATABRICKS_TOKEN env vars
+        # are set as Nextmv secrets)
         w = WorkspaceClient()
-        
+
         # Make sure the job exists
         try:
             w.jobs.get(db_job_id)
-        except Exception:
+        except Exception as err:
             nextmv.log(f"Databricks job with ID {db_job_id} not found")
-            raise Exception(f"Databricks job with ID {db_job_id} not found")
-        
+            raise Exception(f"Databricks job with ID {db_job_id} not found") from err
+
         # Run the job
         run = w.jobs.run_now(job_id=db_job_id).result()
         nextmv.log(f"Created DB run with ID: {run.run_id} for job with ID: {db_job_id}")
@@ -42,7 +41,8 @@ class DecisionFlow(FlowSpec):
     @step
     def return_db_result(run_id: str):
         """Gets the result of the run."""
-        # Authenticate (assumes DATABRICKS_HOST and DATABRICKS_TOKEN env vars are set as Nextmv secrets)
+        # Authenticate (assumes DATABRICKS_HOST and DATABRICKS_TOKEN env vars
+        # are set as Nextmv secrets)
         w = WorkspaceClient()
         # Get the task run ID
         run_output = w.jobs.get_run_output(run_id=run_id)
@@ -54,12 +54,14 @@ class DecisionFlow(FlowSpec):
         result["statistics"]["result"]["custom"]["db_task_run_id"] = run_id
         result["statistics"]["result"]["custom"]["db_job_id"] = options.db_job_id
         return result
-    
+
     # You can add more steps here to enhance the result.
+
 
 def main():
     # There is no input for this workflow
-    client = cloud.Client(api_key=os.getenv("NEXTMV_API_KEY"))
+    # If you need to call a Nextmv App, use the following:
+    # client = cloud.Client(api_key=os.getenv("NEXTMV_API_KEY"))
 
     # Run workflow
     flow = DecisionFlow("DecisionFlow", input=None)
@@ -67,6 +69,7 @@ def main():
     result = flow.get_result(flow.return_db_result)
     # Write out the result
     nextmv.write_local(result)
+
 
 if __name__ == "__main__":
     main()
