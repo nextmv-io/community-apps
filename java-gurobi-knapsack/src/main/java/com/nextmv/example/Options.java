518583c47d4fc56680c0185fe1330b4b608e70f1
Marius Merschformann <marius.merschformann@gmail.com>
2025-06-10 01:02:19 +0200

    Supporting platform style option

--- java-gurobi-knapsack/src/main/java/com/nextmv/example/Options.java
+++ java-gurobi-knapsack/src/main/java/com/nextmv/example/Options.java
@@ -29,6 +29,10 @@ public class Options {
     int duration = 30;
 
     for (int i = 0; i < args.length; ++i) {
+      if (args[i].startsWith("-d=")) {
+        duration = Integer.parseInt(args[i].substring("-d=".length()));
+        continue;
+      }
       switch (args[i]) {
         case "-i":
         case "--input":
