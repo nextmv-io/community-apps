bd5eea5b30487f50cadfb95e1f0941e5198936e6
mooneyc6 <carolyn.mooney@gmail.com>
2025-07-09 10:33:06 -0400

    updates for linting

--- python-xpress-facility-location/main.py
+++ python-xpress-facility-location/main.py
@@ -1,9 +1,9 @@
-import time
+
+import json
 
 import nextmv
-from visuals import draw_sol
 import numpy as np
-import json
+from visuals import draw_sol
 
 try:
     import xpress as xp
@@ -25,18 +25,29 @@ def main() -> None:
     options = nextmv.Options(
         nextmv.Option("input", str, "", "Path to input file. Default is stdin.", False),
         nextmv.Option("output", str, "", "Path to output file. Default is stdout.", False),
-        nextmv.Option("duration", int, 30, "Max runtime duration (in seconds).", False),
+        nextmv.Option("objective", str, "average_distance", "minimizes for average_distance, total_distance, or max_distance", False),
+        nextmv.Option("parks_override", int, 4, "number of parks to build", False),
     )
 
     input = nextmv.load(options=options, path=options.input)
+    if options.objective not in ["average_distance", "total_distance", "max_distance"]:
+        raise ValueError("Invalid objective. Must be either 'average_distance', 'total_distance', or 'max_distance'.")
 
     nextmv.log("Solving facility location problem:")
-    nextmv.log(f"  - schools: {input.data.get('num_schools', [])}")
-    nextmv.log(f"  - sites: {input.data.get('num_sites', 0)}")
-    nextmv.log(f"  - parks: {input.data.get('num_parks', 0)}")
+    nextmv.log(f"  - objective: {options.objective}")
+    nextmv.log(f"  - parks_override: {options.parks_override}")
+    nextmv.log(f"  - schools: {input.data.get('num_schools')}")
+    nextmv.log(f"  - sites: {input.data.get('num_sites')}")
+    nextmv.log(f"  - parks: {input.data.get('num_parks')}")
+
+    np.random.seed(input.data.get('seed'))
 
     SCHOOLS = range(input.data.get('num_schools'))  # set of schools
     SITES = range(input.data.get('num_sites'))      # set of candidate sites
+    if options.parks_override is not None:
+        num_parks = options.parks_override
+    else:
+        num_parks = input.data.get('num_parks')
 
     coord_schools = 10 * np.random.random((input.data.get('num_schools'), 2))  # x-y coordinates between 0 and 10 (in km)
     coord_sites   = 10 * np.random.random((input.data.get('num_sites'), 2))
@@ -51,13 +62,20 @@ def main() -> None:
     build = prob.addVariables(SITES, vartype=xp.binary)
 
     # Objective function and constraints
-    prob.setObjective(xp.Sum(dist[i,j] * serves[i,j] for i in SCHOOLS for j in SITES))
+    if options.objective == "average_distance":
+        prob.setObjective(xp.Sum(dist[i,j] * serves[i,j] for i in SCHOOLS for j in SITES) / input.data.get('num_schools'))
+    elif options.objective == "total_distance":
+        prob.setObjective(xp.Sum(dist[i,j] * serves[i,j] for i in SCHOOLS for j in SITES))
+    elif options.objective == "max_distance":
+        z = prob.addVariable() # add auxiliary variable to the problem
+        prob.addConstraint(z >= xp.Sum(dist[i,j] * serves[i,j] for j in SITES) for i in SCHOOLS)
+        prob.setObjective(z) # replaces the old objective function
 
     # Every school must be served by one park
     prob.addConstraint(xp.Sum(serves[i,j] for j in SITES) == 1 for i in SCHOOLS)
 
     # Exactly n parks are built:
-    prob.addConstraint(xp.Sum(build[j] for j in SITES) == input.data.get('num_parks'))
+    prob.addConstraint(xp.Sum(build[j] for j in SITES) == num_parks)
 
     # Only parks that are built can serve schools
     prob.addConstraint(xp.Sum(serves[i,j] for i in SCHOOLS) <= input.data.get('num_schools') * build[j] for j in SITES)
@@ -71,15 +89,30 @@ def main() -> None:
 
     input.options.provider = "xpress"
 
-    input_charts = draw_sol(n=input.data.get('num_schools'),m=input.data.get('num_sites'), label="Input Chart", coord_schools=coord_schools, coord_sites=coord_sites, SCHOOLS=SCHOOLS, SITES=SITES)
-    output_charts = draw_sol(input.data.get('num_schools'),input.data.get('num_sites'),prob,serves,build, "Output Chart", coord_schools=coord_schools, coord_sites=coord_sites, SCHOOLS=SCHOOLS, SITES=SITES)
+    input_charts = draw_sol(n=input.data.get('num_schools'),
+                            m=input.data.get('num_sites'),
+                            label="Input Chart",
+                            coord_schools=coord_schools,
+                            coord_sites=coord_sites,
+                            SCHOOLS=SCHOOLS,
+                            SITES=SITES)
+    output_charts = draw_sol(input.data.get('num_schools'),
+                             input.data.get('num_sites'),
+                             prob,
+                             serves,
+                             build,
+                             label="Output Chart",
+                             coord_schools=coord_schools,
+                             coord_sites=coord_sites,
+                             SCHOOLS=SCHOOLS,
+                             SITES=SITES)
 
     output = nextmv.Output(
            solution={"solution": solution},
            statistics={"result": {"value": value}, "schema": "v1"},
            assets=[input_charts, output_charts]
        )
-    
+
     nextmv.write(output, path=options.output)
 
 
