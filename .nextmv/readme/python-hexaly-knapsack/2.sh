11936f6869da8568d700bd6e236f7a7e20fc7596
Marius Merschformann <marius.merschformann@gmail.com>
2025-11-19 15:47:09 +0100

    Fixing readme command

--- .nextmv/readme/python-hexaly-knapsack/2.sh
+++ .nextmv/readme/python-hexaly-knapsack/2.sh
@@ -1,3 +1,3 @@
-cat input.json | docker run -i --rm \
+docker run -i --rm \
 -v $(pwd):/app ghcr.io/nextmv-io/runtime/python:3.11 \
-sh -c 'python3 /app/main.py'
+sh -c 'pip install -r /app/requirements.txt && python3 /app/main.py -input input.json -output output.json -duration 30'
